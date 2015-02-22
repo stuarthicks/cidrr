165b971c541669d90681b922f30a53d940a93a96
Stuart Hicks <contact@stuarthicks.me>
2015-02-22 15:25:24 +0000

    update for latest rust nightly

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 // vi: sw=2 ts=2 
+#![feature(env)]
+#![feature(core)]
+use std::env;
+
 fn main() {
-  match std::os::args().as_slice() {
-    [] => println!("need more arguments"),
-    [ref x, ..] if x.as_slice() == "-h" => println!("Help!"),
-    [ref a, ref b] if a.as_slice() == "-c" => println!("{:?}", expand(b.as_slice())),
-    args @ _ => println!("unrecognised arguments: {:?}", args)
-  }
+  let args: Vec<String> = env::args().map(|arg| arg.to_string()).collect();
+  let first_arg: &str = args[1].as_slice();
+  let expanded: &str = expand(first_arg);
+  println!("{:?}", expanded);
 }
 
 fn expand(cidr: &str) -> &str {
-  let foo: &str = cidr.split('/').next().expect("Invalid CIDR");
-  return foo;
+  let bits: &str = cidr.split('/').next().expect("Invalid CIDR");
+  for i in bits.split('.') {
+    println!("{:?}", i)
+  }
+  return bits;
 }
 
 #[test]
