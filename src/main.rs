b26f771cd6e151d5aebcba5b34ea92c22e0f8d7b
Stuart Hicks <contact@stuarthicks.me>
2015-07-08 20:28:57 +0100

    start following community idiom of unit testing within submodile and using assert_eq for better error messages

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,14 @@ fn main() {
     println!("result: {:?}", expanded);
 }
 
-fn expand(cidr: &str) -> String {
+pub fn expand(cidr: &str) -> String {
     let ip: &str = cidr.split('/').next().expect("Invalid CIDR");
     let binary: String = convert_ip_to_binary(ip.to_string());
     println!("{:?}", binary);
-    return binary;
+    return ip.to_string();
 }
 
-fn convert_ip_to_binary(ip: String) -> String {
+pub fn convert_ip_to_binary(ip: String) -> String {
     let mut binary: Vec<String> = Vec::new();
     for i in ip.split('.') {
         let encoded: u8 = u8::from_str_radix(i, 10).ok().unwrap();
@@ -24,7 +24,7 @@ fn convert_ip_to_binary(ip: String) -> String {
     return binary.connect("");
 }
 
-fn convert_binary_to_ip(binary: String) -> String {
+pub fn convert_binary_to_ip(binary: String) -> String {
     let bin: &str = &binary;
     let bin_codepoints: Vec<_> = bin.chars().collect();
     for section in bin_codepoints.chunks(8) {
@@ -35,24 +35,29 @@ fn convert_binary_to_ip(binary: String) -> String {
     return "Not Implemented".to_string();
 }
 
-#[test]
-fn slash_zero_returns_same_ip() {
-    let res = expand("10.1.2.3/0");
-    assert!(res == "10.1.2.3");
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn it_converts_ip_string_to_binary() {
-    assert!(convert_ip_to_binary("10.1.2.3".to_string()) == "00001010000000010000001000000011");
-    assert!(convert_ip_to_binary("127.0.1.1".to_string()) == "01111111000000000000000100000001");
-    assert!(convert_ip_to_binary("192.168.0.1".to_string()) == "11000000101010000000000000000001");
-    assert!(convert_ip_to_binary("169.254.169.254".to_string()) == "10101001111111101010100111111110");
-}
+    #[test]
+    fn slash_zero_returns_same_ip() {
+        let res = expand("10.1.2.3/0");
+        assert_eq!("10.1.2.3", res);
+    }
 
-#[test]
-fn it_converts_binary_to_ip_string() {
-    assert!(convert_binary_to_ip("00001010000000010000001000000011".to_string()) == "10.1.2.3");
-    assert!(convert_binary_to_ip("01111111000000000000000100000001".to_string()) == "127.0.1.1");
-    assert!(convert_binary_to_ip("01111111000000000000000100000001".to_string()) == "192.168.0.1");
-    assert!(convert_binary_to_ip("10101001111111101010100111111110".to_string()) == "169.254.169.254");
+    #[test]
+    fn it_converts_ip_string_to_binary() {
+        assert_eq!("00001010000000010000001000000011", convert_ip_to_binary("10.1.2.3".to_string()));
+        assert_eq!("01111111000000000000000100000001", convert_ip_to_binary("127.0.1.1".to_string()));
+        assert_eq!("11000000101010000000000000000001", convert_ip_to_binary("192.168.0.1".to_string()));
+        assert_eq!("10101001111111101010100111111110", convert_ip_to_binary("169.254.169.254".to_string()));
+    }
+
+    #[test]
+    fn it_converts_binary_to_ip_string() {
+        assert_eq!("10.1.2.3", convert_binary_to_ip("00001010000000010000001000000011".to_string()));
+        assert_eq!("127.0.1.1", convert_binary_to_ip("01111111000000000000000100000001".to_string()));
+        assert_eq!("192.168.0.1", convert_binary_to_ip("01111111000000000000000100000001".to_string()));
+        assert_eq!("169.254.169.254", convert_binary_to_ip("10101001111111101010100111111110".to_string()));
+    }
 }
