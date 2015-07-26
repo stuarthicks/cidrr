e46350df3c9b70fde79042896fdf0b8905fedd91
Stuart Hicks <contact@stuarthicks.me>
2015-07-26 14:05:58 +0100

    rename for shorter methods

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ fn main() {
 
 pub fn expand(cidr: &str) -> String {
     let ip: &str = cidr.split('/').next().expect("Invalid CIDR");
-    let binary: String = convert_ip_to_binary(ip.to_string());
+    let binary: String = ip_to_binary(ip.to_string());
     println!("{:?}", binary);
     return ip.to_string();
 }
 
-pub fn convert_ip_to_binary(ip: String) -> String {
+pub fn ip_to_binary(ip: String) -> String {
     let mut binary: Vec<String> = Vec::new();
     for i in ip.split('.') {
         let encoded: u8 = u8::from_str_radix(i, 10).ok().unwrap();
@@ -24,7 +24,7 @@ pub fn convert_ip_to_binary(ip: String) -> String {
     return binary.connect("");
 }
 
-pub fn convert_binary_to_ip(binary: String) -> String {
+pub fn binary_to_ip(binary: String) -> String {
     let bin: &str = &binary;
     let bin_codepoints: Vec<_> = bin.chars().collect();
     let mut output: Vec<String> = Vec::new();
@@ -47,17 +47,17 @@ mod tests {
 
     #[test]
     fn it_converts_ip_string_to_binary() {
-        assert_eq!("00001010000000010000001000000011", convert_ip_to_binary("10.1.2.3".to_string()));
-        assert_eq!("01111111000000000000000100000001", convert_ip_to_binary("127.0.1.1".to_string()));
-        assert_eq!("11000000101010000000000000000001", convert_ip_to_binary("192.168.0.1".to_string()));
-        assert_eq!("10101001111111101010100111111110", convert_ip_to_binary("169.254.169.254".to_string()));
+        assert_eq!("00001010000000010000001000000011", ip_to_binary("10.1.2.3".to_string()));
+        assert_eq!("01111111000000000000000100000001", ip_to_binary("127.0.1.1".to_string()));
+        assert_eq!("11000000101010000000000000000001", ip_to_binary("192.168.0.1".to_string()));
+        assert_eq!("10101001111111101010100111111110", ip_to_binary("169.254.169.254".to_string()));
     }
 
     #[test]
     fn it_converts_binary_to_ip_string() {
-        assert_eq!("10.1.2.3", convert_binary_to_ip("00001010000000010000001000000011".to_string()));
-        assert_eq!("127.0.1.1", convert_binary_to_ip("01111111000000000000000100000001".to_string()));
-        assert_eq!("192.168.0.1", convert_binary_to_ip("11000000101010000000000000000001".to_string()));
-        assert_eq!("169.254.169.254", convert_binary_to_ip("10101001111111101010100111111110".to_string()));
+        assert_eq!("10.1.2.3", binary_to_ip("00001010000000010000001000000011".to_string()));
+        assert_eq!("127.0.1.1", binary_to_ip("01111111000000000000000100000001".to_string()));
+        assert_eq!("192.168.0.1", binary_to_ip("11000000101010000000000000000001".to_string()));
+        assert_eq!("169.254.169.254", binary_to_ip("10101001111111101010100111111110".to_string()));
     }
 }
